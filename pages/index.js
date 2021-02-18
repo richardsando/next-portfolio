976b53490112dd1a09b19544359f4cbe9bf8aa9f
richardsando <richardsando369@gmail.com>
2021-02-18 17:59:00 +1100

    first commit

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+
+const Page = styled.div`
+  background-color: salmon;
+`;
+
+const Index = () => {
+  return <Page>
+    <h1>Richard Sando</h1>
+    <h1>Full Stack Web Developer</h1>
+  </Page>;
+};
+
+export default Index;
